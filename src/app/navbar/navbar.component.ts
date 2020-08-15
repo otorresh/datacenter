d60d8fed914c355f756a7c9ddf38825a3df38c75
Omar Torres <omtorres@coca-cola.com>
2020-08-14 19:24:07 -0500

    first commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
+
+  refresh() {
+    this.onRefresh.emit();
+  }
+
+}
